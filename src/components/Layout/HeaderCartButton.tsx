d03fd2e3200f2dab92ba0d2adc10fcc7a69b9303
LordCracken <vladislav070201@gmail.com>
2022-10-18 04:32:06 +0300

    add bump animation on cart button on cart items change

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 import { IHeaderCartButton } from '../../interfaces';
@@ -6,12 +6,28 @@ import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = ({ onClick }: IHeaderCartButton) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState<boolean>(false);
   const cartCtx = useContext(CartContext);
 
   const itemsCount = cartCtx.items.reduce((curNum, item) => curNum + item.amount, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+  useEffect(() => {
+    if (!cartCtx.items.length) return;
+
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
