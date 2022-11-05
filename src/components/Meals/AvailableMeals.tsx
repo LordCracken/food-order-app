3fdf105364338049b407bf590e4edce3401a1fb4
LordCracken <vladislav070201@gmail.com>
2022-11-06 01:43:58 +0300

    add error handling for fetching data request

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -9,13 +9,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState<IMealItem[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [httpError, setHttpError] = useState<string>();
 
   useEffect(() => {
     const url = 'https://reactmeals-6aaed-default-rtdb.firebaseio.com/meals.json';
     (async () => {
       const response = await fetch(url);
-      const responseData = await response.json();
 
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
       const loadedMeals = [];
 
       for (const key in responseData) {
@@ -29,13 +34,24 @@ const AvailableMeals = () => {
 
       setMeals(loadedMeals);
       setIsLoading(false);
-    })();
+    })().catch(error => {
+      setIsLoading(false);
+      setHttpError((error as Error).message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes['meals-loading']}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes['meals-error']}>
+        <p>{httpError}</p>
       </section>
     );
   }
